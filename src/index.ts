bdf6e346c41994683364d8d80ddef065cca55890
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-08-17 17:40:32 -0300

    feat:  validation of jobs

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import puppeteer from "puppeteer";
 import { config } from "dotenv";
 import inquirer from "inquirer";
 import { gptPrompt } from "./gpt";
+import { connectDb, prisma, disconnectDB } from "./config";
 config();
 
 (async () => {
+	connectDb();
+	const jobs = await prisma.job.findMany();
+
 	const browser = await puppeteer.launch({
 		executablePath: "/bin/google-chrome-stable",
 		slowMo: 100,
@@ -12,15 +16,22 @@ config();
 		headless: false,
 		defaultViewport: null,
 	});
+	const page = await browser.newPage();
 
-	const answers = await inquirer.prompt({
-		type: "input",
-		message: "build CV? (y/n)",
-		name: "validation",
-	});
+	for (let i in jobs) {
+		await page.goto(jobs[i].url);
 
-	if(answers.validation === "y") console.log('true');
-	if(answers.validation === "n") console.log('false');
+		const answers = await inquirer.prompt({
+			type: "list",
+			message: "build CV?\n",
+			name: "validation",
+			choices: ["yes", "no", "exit"],
+		});
+
+		if (answers.validation === "yes") console.log("true");
+		if (answers.validation === "no") continue;
+		if (answers.validation === "exit") break;
+	}
 
 	browser.close();
 })();
