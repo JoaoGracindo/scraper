d4164571b98d0cd7d79e5149ecec6eca9ded7060
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-08-17 14:04:57 -0300

    Feat: open ai chat completion

--- src/gpt.ts
+++ src/gpt.ts
@@ -1,7 +1,10 @@
-import { OpenAIApi } from "openai";
+import { OpenAIApi, Configuration } from "openai";
 import { config } from "dotenv";
 config();
 
-const openai = new OpenAIApi();
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+const openai = new OpenAIApi(configuration);
 
 export {openai};
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
 import puppeteer from "puppeteer";
+import { openai } from "./gpt";
+import { config } from "dotenv";
+config();
 
 (async () => {
-	const browser = await puppeteer.launch({
-		executablePath: "/bin/google-chrome-stable",
-		slowMo: 100,
-		userDataDir: process.env.USER_DIR,
-		headless: false,
-		defaultViewport: null,
-	});
+	// const browser = await puppeteer.launch({
+	// 	executablePath: "/bin/google-chrome-stable",
+	// 	slowMo: 100,
+	// 	userDataDir: process.env.USER_DIR,
+	// 	headless: false,
+	// 	defaultViewport: null,
+	// });
 
-	browser.close();
+	const completion = await openai.createChatCompletion({
+		messages: [{ 
+			role: "system",
+			content: "hellow world",
+			 }],
+		model: "gpt-3.5-turbo",
+	})
+
+	console.log(completion.data.choices[0].message)
+
+	// browser.close();
 })();
