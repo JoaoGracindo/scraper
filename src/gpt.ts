dcd9ac0cf6e648462008c3edbc343a73d692f5e8
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-08-22 17:08:54 -0300

    feat: gpt prompt

--- src/gpt.ts
+++ src/gpt.ts
@@ -1,5 +1,6 @@
 import { OpenAIApi, Configuration } from "openai";
 import { config } from "dotenv";
+import { Job } from "@prisma/client";
 config();
 
 const configuration = new Configuration({
@@ -7,16 +8,23 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-async function gptPrompt() {
-	return await openai.createChatCompletion({
+async function gptPrompt(job: Job) {
+	const chatCompletion = await openai.createChatCompletion({
 		messages: [
 			{
 				role: "system",
-				content: "hellow world",
+				content: "voce é um programa que analisa vaga de emprego de desenvolvedor de software.",
+			},
+			{
+				role: "user",
+				content: `liste os requesitos e outras informações importantes desta vaga: ${job.jobDescription}`
 			},
 		],
 		model: "gpt-3.5-turbo",
 	});
+
+	const response = chatCompletion.data.choices;
+	return response;
 }
 
 export { openai, gptPrompt };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import puppeteer from "puppeteer";
 import { config } from "dotenv";
 import inquirer from "inquirer";
+import { createSpinner } from "nanospinner"
 import { gptPrompt } from "./gpt";
 import { connectDb, prisma, disconnectDB } from "./config";
 config();
@@ -8,30 +9,50 @@ config();
 (async () => {
 	connectDb();
 	const jobs = await prisma.job.findMany();
-	disconnectDB();
-	const browser = await puppeteer.launch({
-		executablePath: "/bin/google-chrome-stable",
-		slowMo: 100,
-		userDataDir: process.env.USER_DIR,
-		headless: false,
-		defaultViewport: null,
-	});
-	const page = await browser.newPage();
-
+	
 	for (let i in jobs) {
-		await page.goto(jobs[i].url);
-
+		const spinner = createSpinner(" Waiting for gpt response.\n").start();
+		const summary = await gptPrompt(jobs[i]);
+		spinner.success().clear();
+		console.log(summary, "\n\n");
 		const answers = await inquirer.prompt({
 			type: "list",
 			message: "build CV?\n",
 			name: "validation",
 			choices: ["yes", "no", "exit"],
 		});
-
-		if (answers.validation === "yes") console.log("true");
+		
+		await prisma.job.update({
+			where: {
+				id: jobs[i].id
+			},
+			data: {
+				analyzed: true
+			}
+		})
 		if (answers.validation === "no") continue;
 		if (answers.validation === "exit") break;
+		
+		prisma.job.update({
+			where: {
+				id: jobs[i].id
+			},
+			data: {
+				approved: true
+			}
+		})
+		const browser = await puppeteer.launch({
+			executablePath: "/bin/google-chrome-stable",
+			slowMo: 100,
+			userDataDir: process.env.USER_DIR,
+			headless: false,
+			defaultViewport: null,
+		});
+		const linkedinPage = await browser.newPage();
+		linkedinPage.goto(jobs[i].url);
+		
+		
 	}
-
-	browser.close();
+	
+	disconnectDB();
 })();
